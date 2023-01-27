5193a2c0611948d0e37bac4966226227e34143b6
Salmenraju <rajusalmen4@gmail.com>
2023-01-27 13:29:49 +0530

    First Version of Application

---
+++ src/test/java/com/salmen/demo/NewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salmen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
