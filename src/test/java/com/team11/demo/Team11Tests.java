122826f494cf074083eababbbacb7133f5541e49
harsav360 <annu7418@gmail.com>
2023-02-07 12:29:51 +0530

    Updated Version of Application

---
+++ src/test/java/com/team11/demo/Team11Tests.java
@@ -0,0 +1,13 @@
+package com.team11.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team11Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
